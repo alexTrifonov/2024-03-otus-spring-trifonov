cc9935654eea2afd2b45cf9c5fe5b84bae9501f0
Alex <nudniy.slava@yandex.ru>
2025-02-15 18:38:11 +0300

    fix

--- hw10-spring-spa/hw-10-back/src/main/java/ru/otus/hw/controllers/BookController.java
+++ hw10-spring-spa/hw-10-back/src/main/java/ru/otus/hw/controllers/BookController.java
@@ -5,51 +5,43 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import ru.otus.hw.dto.BookDto;
-import ru.otus.hw.models.Book;
 import ru.otus.hw.services.BookService;
 import ru.otus.hw.exceptions.EntityNotFoundException;
 
 import java.util.List;
-import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
-@Controller
+@RestController
 public class BookController {
 
     private final BookService bookService;
 
     @GetMapping("/api/books")
     public ResponseEntity<List<BookDto>> getAllBooks() {
-        List<BookDto> books = bookService.findAll().stream()
-                .map(BookDto::fromBook).toList();
-        return ResponseEntity.ok(books);
+        return ResponseEntity.ok(bookService.findAll().stream()
+                .map(BookDto::fromBook).toList());
     }
 
     @GetMapping("/api/books/{id}")
     public ResponseEntity<BookDto> getBook(@PathVariable("id") long id) {
-        BookDto bookDto;
-        Optional<Book> optionalBook = bookService.findById(id);
-        if (optionalBook.isPresent()) {
-            bookDto = BookDto.fromBook(optionalBook.get());
-        } else {
-            throw new EntityNotFoundException("Book not found");
-        }
-        return ResponseEntity.ok(bookDto);
+        return bookService.findById(id)
+                .map(BookDto::fromBook)
+                .map(ResponseEntity::ok)
+                .orElseThrow(() -> new EntityNotFoundException("Book with id " + id + " not found"));
     }
 
     @PostMapping("/api/books")
     public ResponseEntity<BookDto> addBook(@Valid @RequestBody BookDto bookDto) {
-        Book savedBook = bookService.update(bookDto.toBook());
-        return ResponseEntity.ok(BookDto.fromBook(savedBook));
+        return ResponseEntity.ok(BookDto.fromBook(bookService.update(bookDto.toBook())));
     }
 
     @DeleteMapping("/api/books/{id}")
@@ -60,7 +52,6 @@ public class BookController {
 
     @PutMapping("/api/books/{id}")
     public ResponseEntity<BookDto> updateBook(@Valid @RequestBody BookDto bookDto) {
-        Book updatedBook = bookService.update(bookDto.toBook());
-        return ResponseEntity.ok(BookDto.fromBook(updatedBook));
+        return ResponseEntity.ok(BookDto.fromBook(bookService.update(bookDto.toBook())));
     }
 }
--- hw10-spring-spa/hw-10-back/src/main/java/ru/otus/hw/controllers/CommentController.java
+++ hw10-spring-spa/hw-10-back/src/main/java/ru/otus/hw/controllers/CommentController.java
@@ -3,15 +3,15 @@ package ru.otus.hw.controllers;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import ru.otus.hw.dto.CommentDto;
 import ru.otus.hw.services.CommentService;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @Slf4j
 public class CommentController {
@@ -20,8 +20,7 @@ public class CommentController {
 
     @GetMapping("/api/comments")
     public ResponseEntity<List<CommentDto>> listPage(@RequestParam("bookId") long bookId) {
-        List<CommentDto> comments = commentService.findByBookId(bookId).stream()
-                .map(CommentDto::fromComment).toList();
-        return ResponseEntity.ok(comments);
+        return ResponseEntity.ok(commentService.findByBookId(bookId).stream()
+                .map(CommentDto::fromComment).toList());
     }
 }
